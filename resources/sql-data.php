87e4ced68efacda8346bb09631d8a99a97b71639
Buğra Özkan <38759414+xozkann@users.noreply.github.com>
2018-11-26 19:18:38 +0300

    Add files via upload

--- resources/sql-data.php
+++ resources/sql-data.php
@@ -5,4 +5,6 @@ require 'connect.php';
 $kullanicilar = $db->query('SELECT * FROM vt_kullanicilar WHERE vt_durum = 1', PDO::FETCH_ASSOC);
 
 $lisanslar = $db->query('SELECT * FROM vt_lisanslar', PDO::FETCH_ASSOC);
+
+$ihbarlar = $db->query('SELECT * FROM vt_ihbarlar', PDO::FETCH_ASSOC);
 ?>
